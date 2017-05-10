9013104e4491ee2c5e7d08410d0a6743c23696e1
hdh <smoothiefrog@gmail.com>
2017-05-10 18:50:35 +0800

    web

--- EX09/index.js
+++ EX09/index.js
@@ -47,7 +47,7 @@ function SearchNearby() {
 SearchNearby();
 
 
-var pos = [{
+var posi = [{
 	lng: 120.01429,
 	lat: 30.295116
 }, {
@@ -60,3 +60,48 @@ var pos = [{
 	lng: 120.019213,
 	lat: 30.297298
 }, ];
+var pos = [];
+
+var urls = [
+	"img/gym.jpg",
+	"img/dinning.jpg",
+	"img/canteen.jpg",
+	"img/classroom.jpg",
+];
+var images = [];
+for (var i = 0; i < posi.length; ++i) {
+	images.push(document.createElement('img'));
+	images[i].src = urls[i];
+	images[i].style = 'width: 200px; height: 100%;';
+	
+}
+var texts = [
+	"<div>杭州师范大学体育场</div><div style='font-size: 13px; font-weight: bold;'></div>",
+	"<div>杭州师范大学食堂</div><div style='font-size: 13px; font-weight: bold;'></div>",
+	"<div>杭州师范大学博文苑</div><div style='font-size: 13px; font-weight: bold;'></div>",
+	"<div>杭州师范大学恕园</div><div style='font-size: 13px; font-weight: bold;'></div>",
+];
+
+var infoStyle = [];
+var infoWindows = [];
+for (var i = 0; i < posi.length; ++i) {
+	var div = document.createElement('div');
+	div.appendChild(images[i]);
+	div.innerHTML += texts[i];
+	infoStyle.push(div);
+	pos.push(new BMap.Point(posi[i].lng, posi[i].lat));
+	infoWindows.push(new BMap.InfoWindow(infoStyle[i]));
+}
+for (var i = 0; i < pos.length; ++i) {
+	var marker = new BMap.Marker(pos[i]);
+	marker.disableMassClear();
+
+	(function(index) {
+		marker.addEventListener("click", function(e) {
+			e.target.openInfoWindow(infoWindows[index]);
+		});
+	})(i);
+
+	map.addOverlay(marker);
+	marker.setAnimation(BMAP_ANIMATION_BOUNCE);
+}
